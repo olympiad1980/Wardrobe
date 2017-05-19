daed91ba72201580d50b618eb39effee0cc2d65b
admin <olympiad1980@gmail.com>
2017-05-19 11:18:56 +0300

    Navigation

---
+++ Wardrobe/NavigationStoryVC.swift
@@ -0,0 +1,12 @@
+
+import UIKit
+
+class NavigationStoryVC: UINavigationController {
+    let navigation = UINavigationBar()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigation.backgroundColor = UIColor.black
+        
+    }
+}
